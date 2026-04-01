f24c9f4431bdc5cd19660b26949ccba18a398826
SajanaKavishan <trsajanakavishan67@gmail.com>
2026-04-01 13:11:14 +0530

    Add updateUserProfile route to userRoutes for profile updates

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser, getMe } = require('../controllers/userController');
+const { registerUser, loginUser, getMe, updateUserProfile } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware'); // Import the protect middleware to secure the /me route   
 
 // Routes
@@ -8,5 +8,6 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 // This route is protected by the protect middleware, which means that only authenticated users can access it. The getMe function will return the profile information of the logged-in user.
 router.get('/me', protect, getMe); 
+router.put('/profile', protect, updateUserProfile);
 
 module.exports = router;
